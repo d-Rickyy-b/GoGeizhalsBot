e910e50081f6cf2379fc77c5814c6ae1aa4b9639
Rico <d-Rickyy-b@users.noreply.github.com>
2021-12-14 02:39:42 +0100

    feat: implement priceagent struct

---
+++ internal/bot/priceagent.go
@@ -0,0 +1,20 @@
+package bot
+
+import "GoGeizhalsBot/internal/geizhals"
+
+type PriceAgent struct {
+	ID                   string               `json:"id"`
+	Name                 string               `json:"name"`
+	Entity               geizhals.Entity      `json:"entity"`
+	NotificationSettings NotificationSettings `json:"notificationSettings"`
+}
+
+type NotificationSettings struct {
+	NotifyAlways    bool    `json:"notifyAlways"`
+	NotifyPriceDrop bool    `json:"notifyPriceDrop"`
+	NotifyPriceRise bool    `json:"notifyPriceRise"`
+	NotifyAbove     bool    `json:"notifyAbove"`
+	NotifyBelow     bool    `json:"notifyBelow"`
+	AbovePrice      float32 `json:"abovePrice"`
+	BelowPrice      float32 `json:"belowPrice"`
+}
