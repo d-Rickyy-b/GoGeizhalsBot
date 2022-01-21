a977fccc723f878fca2ccb0f53a32e2ce4a994aa
Rico <d-Rickyy-b@users.noreply.github.com>
2022-01-21 03:50:52 +0100

    fix: randomize proxy on initial load

--- internal/geizhals/proxy.go
+++ internal/geizhals/proxy.go
@@ -1,23 +1,21 @@
 package geizhals
 
 import (
-	"log"
+	"math/rand"
 	"net/url"
 )
 
 var proxies []*url.URL
 
 // InitProxies initializes the proxy list.
-func InitProxies(p []string) {
-	// TODO scramble proxies
-	for _, proxy := range p {
-		parsedProxy, parseErr := url.Parse(proxy)
-		if parseErr != nil {
-			log.Println("Can't parse proxy!", parseErr)
-			continue
-		}
-		proxies = append(proxies, parsedProxy)
+func InitProxies(p []*url.URL) {
+	// Randomize order of array
+	for i := range p {
+		j := rand.Intn(i + 1) //nolint:gosec
+		p[i], p[j] = p[j], p[i]
 	}
+
+	proxies = p
 }
 
 // getNextProxy returns the next proxy from the list. Proxies are cycled so that
