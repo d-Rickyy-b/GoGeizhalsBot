d282404f23f6b0aaa9e7f11311072c696a4e6c3e
Rico <d-Rickyy-b@users.noreply.github.com>
2022-12-16 04:37:29 +0100

    refactor: improve code style

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	var configFile = flag.String("config", "config.yml", "Path to config file")
+	configFile := flag.String("config", "config.yml", "Path to config file")
 	flag.Parse()
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
